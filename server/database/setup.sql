2313ba32b28d676fc7abc478f7762f87f44460c0
Sidique <sidiqueakim@gmail.com>
2023-10-30 10:49:12 +0000

    added the users tables to the database

--- server/database/setup.sql
+++ server/database/setup.sql
@@ -1,1 +1,11 @@
 -- SQL Script
+drop table if exists users;
+
+CREATE TABLE users (
+    id INT GENERATED ALWAYS AS IDENTITY, 
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,  -- usually you'd store a hashed version of the password
+    email VARCHAR(255) UNIQUE,
+    date_registered TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
