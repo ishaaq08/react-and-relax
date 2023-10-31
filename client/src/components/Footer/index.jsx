8f8fc791f41365fa03d83fdbe315d368caa6f415
JJatau <jjatau14@outlook.com>
2023-10-31 10:03:32 +0000

    animated footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -6,41 +6,70 @@ import {
   Twitter,
   Youtube,
 } from '../../assets/Icons';
+import { Reveal } from '../Reveal';
+import { motion } from 'framer-motion';
 
 const index = () => {
   return (
     <div className="bg-[#00B4D8] p-5 w-full text-white">
       <div className="flex justify-between items-center mb-6">
         <div className="text-white font-semibold text-5xl tracking-widest">
-          <h2>React + Relax</h2>
+          <Reveal>
+            <h2>React + Relax</h2>
+          </Reveal>
         </div>
         <div className="flex gap-7">
           {/* First Row */}
           <div>
-            <h4 className="font-semibold text-lg tracking-widest">Help</h4>
+            <Reveal>
+              <h4 className="font-semibold text-lg tracking-widest">Help</h4>
+            </Reveal>
             <div className="flex flex-col gap-1">
-              <Link to="/">Help & FAQ</Link>
+              <Reveal>
+                <Link to="/">Help & FAQ</Link>
+              </Reveal>
             </div>
           </div>
           {/* Second Row */}
           <div>
-            <h4 className="font-semibold text-lg tracking-widest">
-              Legal Info
-            </h4>
+            <Reveal>
+              <h4 className="font-semibold text-lg tracking-widest">
+                Legal Info
+              </h4>
+            </Reveal>
             <div className="flex flex-col gap-1">
-              <Link to="/">Privacy Policy</Link>
-              <Link to="/">Terms & Conditions</Link>
-              <Link to="/">Cookie Polciy</Link>
+              <Reveal>
+                <Link to="/">Privacy Policy</Link>
+              </Reveal>
+              <Reveal>
+                <Link to="/">Terms & Conditions</Link>
+                <Reveal></Reveal>
+
+                <Link to="/">Cookie Polciy</Link>
+              </Reveal>
             </div>
           </div>
           {/* Third Row */}
           <div>
-            <h4 className="font-semibold text-lg tracking-widest">Follow Us</h4>
+            <Reveal>
+              <h4 className="font-semibold text-lg tracking-widest">
+                Follow Us
+              </h4>
+            </Reveal>
             <div className="flex flex-col gap-1">
-              <Link to="/">Facebook</Link>
-              <Link to="/">Twitter</Link>
-              <Link to="/">Instagram</Link>
-              <Link to="/">Tiktok</Link>
+              <Reveal>
+                <Link to="/">Facebook</Link>
+              </Reveal>
+              <Reveal>
+                <Link to="/">Twitter</Link>
+              </Reveal>
+              <Reveal>
+                <Link to="/">Instagram</Link>
+              </Reveal>
+
+              <Reveal>
+                <Link to="/">Tiktok</Link>
+              </Reveal>
             </div>
           </div>
         </div>
@@ -48,18 +77,38 @@ const index = () => {
 
       <div className="w-full py-5 ">
         <div className="flex items-center justify-center">
-          <span className="flex  bg-white w-4/12">
+          <motion.span
+            initial={{ opacity: 0, y: '20vh' }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="flex  bg-white w-4/12"
+          >
             <hr />
-          </span>
+          </motion.span>
+
           <div className="flex px-10 justify-around flex-1">
-            <Facebook />
-            <Twitter />
-            <Youtube />
-            <TikTok />
+            <Reveal>
+              <Facebook />
+            </Reveal>
+            <Reveal>
+              <Twitter />
+            </Reveal>
+            <Reveal>
+              <Youtube />
+            </Reveal>
+            <Reveal>
+              <TikTok />
+            </Reveal>
           </div>
-          <span className="flex  bg-white w-4/12">
+
+          <motion.span
+            initial={{ opacity: 0, y: '20vh' }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="flex bg-white w-4/12"
+          >
             <hr />
-          </span>
+          </motion.span>
         </div>
       </div>
     </div>
