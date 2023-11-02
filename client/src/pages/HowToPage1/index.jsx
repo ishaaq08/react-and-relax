295941e7e042a6d953f33d1f31a096bc3b0ce673
JJatau <jjatau14@outlook.com>
2023-11-02 13:06:02 +0000

    styling profile page

--- client/src/pages/HowToPage1/index.jsx
+++ client/src/pages/HowToPage1/index.jsx
@@ -1,13 +1,22 @@
 import { useEffect } from 'react';
 import { BeginGame, BreakDurationForm, HowToPlay } from '../../components';
 import { useData } from '../../contexts';
+import background5 from '../../assets/background5.svg';
+
+const bgStyles = {
+  backgroundImage: `url(${background5})`, // Use the 'url' function to specify the image path // Adjust to your preference
+  backgroundSize: 'cover',
+  backgroundPosition: 'center', // Adjust to your preference
+  // backgroundRepeat: 'no-repeat',
+  height: '100vh',
+  // Set the desired height of your hero section
+};
 
 const index = () => {
-
-  const { setSession } = useData()
+  const { setSession } = useData();
   useEffect(() => {
-    setSession(1)
-  },[])
+    setSession(1);
+  }, []);
 
   return (
     <div className="bg-[#023E8A] h-screen">
